4ad42728b8ccb26cbfa053827e9156ccbcd04d19
Paul Maodi <paulmaodi2001@gmail.com>
2023-07-28 18:21:16 +0200

    First commit

---
+++ uMlandeli/app/src/main/java/za/co/umlandeli/Splash_screen.java
@@ -0,0 +1,14 @@
+package za.co.umlandeli;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Splash_screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
